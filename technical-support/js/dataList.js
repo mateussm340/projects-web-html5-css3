d425f45a57af187c8f12395789684d6f964800a1
mateussm340 <mateussm340@gmail.com>
2025-08-29 10:04:31 -0300

    feat: add list visibility on tech support home page

---
+++ technical-support/js/dataList.js
@@ -0,0 +1,22 @@
+export const devicesList = [
+    {
+        id: 1, 
+        title: 'TV',
+        imgLink: 'Q7DK2Whp/icon-tv.png'
+    },
+    {
+        id: 2, 
+        title: 'Computadores',
+        imgLink: 'V0fsJcKQ/icon-desktop.png'
+    },
+    {
+        id: 3, 
+        title: 'Celulares e Tablets',
+        imgLink: 'vxw9tCj8/icon-smartphone.png'
+    }
+];
+
+export const itemsFooterList = [
+    'Idioma', 'Dispositivos compatíveis', 'Contrato de assinatura', 'Politica de privacidade',
+    'Proteção de dados no Brasil', 'Anuncios personalizados', 'Ajuda'
+];
---
+++ technical-support/js/viewList.js
@@ -0,0 +1,25 @@
+import { devicesList, itemsFooterList } from "./dataList.js";
+
+function showDevicesListOnScreen() {
+    const elementDevice = document.querySelector('#element-device');
+
+    devicesList.forEach((device) => {
+        elementDevice.innerHTML += `
+            <li class="menu__item model__vertical model__center">
+                <img src="https://i.ibb.co/${device.imgLink}" alt="Icone - ${device.title}" class="container__image"/>
+                <h2 class="container__subtitle">${device.title}</h2>
+            </li>
+        `;
+    });
+}
+
+function showItemsFooterList() {
+    const elementItemFooter = document.querySelector('#element-item-footer');
+
+    itemsFooterList.forEach((itemFooter, index) => {
+        elementItemFooter.innerHTML += `<li id="item-footer-${index}" class="container__text">${itemFooter}</li>`;
+    });
+}
+
+showDevicesListOnScreen();
+showItemsFooterList();
